844d9d196a147d85b96ecac80e51d7d2af133abf
ZRE <chy853@gmail.com>
2025-02-28 09:05:39 +0000

    Update example.sh to format script and source_git_url as comments

--- example.sh
+++ example.sh
@@ -12,7 +12,7 @@ do
     valgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>"build/valgrind/$(basename $test).log" $test > /dev/null
 done
 
-{
-    "script": "#!/bin/bash\n\ncmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DFETCH_GOOGLETEST=OFF\ncmake --build build --clean-first\n\nmkdir -p build/grp\nmkdir -p build/valgrind\n\nfor test in $(find build -name \"ut*\" ! -name \"*.*\")\ndo\n$test --gtest_output=json:\"build/grp/$(basename $test).json\" > /dev/null; echo Return $?\nvalgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>\"build/valgrind/$(basename $test).log\" $test > /dev/null\ndone",
-    "source_git_url": "zre/test0127"
-}
+# {
+#     "script": "#!/bin/bash\n\ncmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DFETCH_GOOGLETEST=OFF\ncmake --build build --clean-first\n\nmkdir -p build/grp\nmkdir -p build/valgrind\n\nfor test in $(find build -name \"ut*\" ! -name \"*.*\")\ndo\n$test --gtest_output=json:\"build/grp/$(basename $test).json\" > /dev/null; echo Return $?\nvalgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>\"build/valgrind/$(basename $test).log\" $test > /dev/null\ndone",
+#     "source_git_url": "zre/test0127"
+# }
