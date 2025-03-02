4123c8048a06cee694a1ff44142309f5a58c5414
ZRE <chy853@gmail.com>
2025-03-02 14:04:19 +0000

    Add Python script execution to example.sh for parsing test results

--- example.sh
+++ example.sh
@@ -11,8 +11,9 @@ do
     $test --gtest_output=json:"build/grp/$(basename $test).json" > /dev/null; echo Return $?
     valgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>"build/valgrind/$(basename $test).log" $test > /dev/null
 done
+python3 utils/grp_parser.py $CODE_PATH/build/grp/ut_all.json
 
 # {
-#     "script": "#!/bin/bash\n\ncmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DFETCH_GOOGLETEST=OFF\ncmake --build build --clean-first\n\nmkdir -p build/grp\nmkdir -p build/valgrind\n\nfor test in $(find build -name \"ut*\" ! -name \"*.*\")\ndo\n$test --gtest_output=json:\"build/grp/$(basename $test).json\" > /dev/null; echo Return $?\nvalgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>\"build/valgrind/$(basename $test).log\" $test > /dev/null\ndone",
+#     "script": "#!/bin/bash\n\ncmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DFETCH_GOOGLETEST=OFF\ncmake --build build --clean-first\n\nmkdir -p build/grp\nmkdir -p build/valgrind\n\nfor test in $(find build -name \"ut*\" ! -name \"*.*\")\ndo\n$test --gtest_output=json:\"build/grp/$(basename $test).json\" > /dev/null; echo Return $?\nvalgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>\"build/valgrind/$(basename $test).log\" $test > /dev/null\ndone\npython3 utils/grp_parser.py $CODE_PATH/build/grp/ut_all.json",
 #     "source_git_url": "zre/test0127"
 # }
