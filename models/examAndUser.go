e98d583701428845e2aa77e739e12319c325248a
ZRE <chy853@gmail.com>
2025-03-09 22:08:05 +0800

    Add Question reference to ExamAndUser model for improved data association

--- models/examAndUser.go
+++ models/examAndUser.go
@@ -1,11 +1,12 @@
 package models
 
 type ExamAndUser struct {
-	ID         uint `gorm:"primarykey"`
-	ExamID     uint `gorm:"not null"`
-	Exam       Exam `gorm:"foreignKey:ExamID"`
-	UserID     uint `gorm:"not null"`
-	User       User `gorm:"foreignKey:UserID"`
-	QuestionID uint `gorm:"not null"`
-	Score      int  `gorm:"not null"`
+	ID         uint     `gorm:"primarykey"`
+	ExamID     uint     `gorm:"not null"`
+	Exam       Exam     `gorm:"foreignKey:ExamID"`
+	UserID     uint     `gorm:"not null"`
+	User       User     `gorm:"foreignKey:UserID"`
+	QuestionID uint     `gorm:"not null"`
+	Question   Question `gorm:"foreignKey:QuestionID"`
+	Score      int      `gorm:"not null"`
 }
