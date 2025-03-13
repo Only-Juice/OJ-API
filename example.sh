23d4da6aa537f7ac4ec327a8493775a04e6f4888
ZRE <chy853@gmail.com>
2025-03-13 03:22:16 +0000

    Update SANDBOX_COUNT in environment configuration and modify example script source_git_url

--- example.sh
+++ example.sh
@@ -15,5 +15,5 @@ python3 utils/grp_parser.py $CODE_PATH/build/grp/ut_all.json
 
 # {
 #     "script": "#!/bin/bash\n\ncmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DFETCH_GOOGLETEST=OFF\ncmake --build build --clean-first\n\nmkdir -p build/grp\nmkdir -p build/valgrind\n\nfor test in $(find build -name \"ut*\" ! -name \"*.*\")\ndo\n$test --gtest_output=json:\"build/grp/$(basename $test).json\" > /dev/null; echo Return $?\nvalgrind --error-exitcode=2 --track-origins=yes --leak-check=full --log-fd=9 9>\"build/valgrind/$(basename $test).log\" $test > /dev/null\ndone\npython3 utils/grp_parser.py $CODE_PATH/build/grp/ut_all.json",
-#     "source_git_url": "zre/test0127"
+#     "source_git_url": "zre/OOP2023f_111590028_HW"
 # }
