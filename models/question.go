2db4fab16631a85457b9c969b9a879aeb506ee32
ZRE <chy853@gmail.com>
2025-03-31 00:40:05 +0800

    Add startTime and endTime fields to Question model and update API documentation

--- models/question.go
+++ models/question.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type Question struct {
-	ID          uint   `gorm:"primaryKey"`
-	Title       string `gorm:"size:100;not null"`
-	Description string `gorm:"size:5000;not null"`
-	GitRepoURL  string `gorm:"size:250"`
+	ID          uint      `gorm:"primaryKey"`
+	Title       string    `gorm:"size:100;not null"`
+	Description string    `gorm:"size:5000;not null"`
+	GitRepoURL  string    `gorm:"size:250"`
+	StartTime   time.Time `gorm:"not null"`
+	EndTime     time.Time `gorm:"not null"`
 }
