3e71cdecf6b9000d0ac8b9969084e2c5ffce8a8b
ZRE <chy853@gmail.com>
2025-09-11 11:17:28 +0800

    fix database migrations

--- models/userQuestionTable.go
+++ models/userQuestionTable.go
@@ -9,6 +9,6 @@ type UserQuestionTable struct {
 	Score     float64              `gorm:"not null" json:"score"`
 	JudgeTime time.Time            `gorm:"not null;default:CURRENT_TIMESTAMP" json:"judge_time" example:"2006-01-02T15:04:05Z07:00" time_format:"RFC3339"`
 	Message   string               `gorm:"not null" json:"message"`
+	Commit    string               `gorm:"size:150;not null;default:''" json:"commit"`
 	CreatedAt time.Time            `gorm:"autoCreateTime" json:"created_at"`
-	Commit    string               `gorm:"size:150;not null" json:"commit"`
 }
