212d35fd357e2d7b51e592bf1a25247903e047bc
ZRE <chy853@gmail.com>
2025-09-14 23:04:17 +0800

    sql index

--- models/userQuestionTable.go
+++ models/userQuestionTable.go
@@ -4,11 +4,11 @@ import "time"
 
 type UserQuestionTable struct {
 	ID        uint                 `gorm:"primaryKey" json:"id"`
-	UQRID     uint                 `gorm:"not null" json:"uqr_id"`
+	UQRID     uint                 `gorm:"not null;index:idx_uqt_uqr_score_created,priority:1" json:"uqr_id"`
 	UQR       UserQuestionRelation `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"uqr"`
-	Score     float64              `gorm:"not null" json:"score"`
+	Score     float64              `gorm:"not null;index:idx_uqt_uqr_score_created,priority:2" json:"score"`
 	JudgeTime time.Time            `gorm:"not null;default:CURRENT_TIMESTAMP" json:"judge_time" example:"2006-01-02T15:04:05Z07:00" time_format:"RFC3339"`
 	Message   string               `gorm:"not null" json:"message"`
 	Commit    string               `gorm:"size:150;not null;default:''" json:"commit"`
-	CreatedAt time.Time            `gorm:"autoCreateTime" json:"created_at"`
+	CreatedAt time.Time            `gorm:"autoCreateTime;index:idx_uqt_uqr_score_created,priority:3" json:"created_at"`
 }
